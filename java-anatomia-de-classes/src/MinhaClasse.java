f691e0419b8a3d44b7f815730ec8f35ad2f228a9
Roberson Mendes <roberson.mendes.c@gmail.com>
2023-02-22 21:23:11 -0300

    doc: class "anatomia de classes aula 3"
    - Notes about naming conventions.

--- java-anatomia-de-classes/src/MinhaClasse.java
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -1,5 +1,12 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.print("Hello World!");
+
+        /* Regras de nomenclatura:
+         * 1: camel case
+         * 2: Se for uma constante, nomes compostos são separados por "_"
+         * 3: Variáveis podem conter apenas os caracteres "_, $, letras e números de 0 a 0"
+         * 4: Variáveis podem começar somente com "_, $ ou letras" (nunca com números)
+         */
     }
 }
